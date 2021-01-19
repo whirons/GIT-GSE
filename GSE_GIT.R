7f04668029336859c5f2646cd8c4371a324c52f7
whirons <nkh20stu@uea.ac.uk>
2021-01-19 15:57:25 +0000

    set up packages

--- GSE_GIT.R
+++ GSE_GIT.R
@@ -2,3 +2,15 @@
 #git test
 #test2
 #pull test
+
+#install packages
+install.packages("move")
+install.packages("mapview")
+install.packages("dplyr")
+install.packages("Amt")
+
+#libary 
+library(move)
+library(mapview)
+library(dplyr)
+library(amt)
