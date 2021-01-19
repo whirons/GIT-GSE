16fe8e3fdf20fd3934bf6b90acf8d5b52d531895
whirons <nkh20stu@uea.ac.uk>
2021-01-19 16:48:20 +0000

    Intial set up

--- GSE_GIT.R
+++ GSE_GIT.R
@@ -8,9 +8,32 @@ install.packages("move")
 install.packages("mapview")
 install.packages("dplyr")
 install.packages("Amt")
+install.packages("tidyverse")
 
 #libary 
 library(move)
 library(mapview)
 library(dplyr)
 library(amt)
+library(knitr)
+library(lubridate)
+library(maptools)
+library(raster)
+library(ggmap)
+library(tibble)
+library(leaflet)
+library(tidyverse)
+
+#Get movebank data for required time stamps
+loginStored <- movebankLogin(username="WillHirons", password="Cricket3219")
+
+GSE.move <- getMovebankData(study="Great Spotted Eagle - Belarus", 
+                               login=loginStored,  timestamp_start="20190425193000000",
+                            timestamp_end="20190729180000000")
+head(GSE.move) 
+
+GSE.dat <- as(GSE.move, "data.frame")
+
+GSE.dat$timestamp<-as.POSIXct(GSE.dat$timestamp, format="%Y-%m-%d %H:%M:%OS", tz="UTC")
+
+#Read in 2019 Camtrap dataset
